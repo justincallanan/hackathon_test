591db1f80ea3b7fbb71887a67ccd7d0f14345eb6
justincallanan <131004025+justincallanan@users.noreply.github.com>
2023-04-18 13:14:47 -0400

    Update fileextension.py

--- hackathon-2023-boston-main/Scraper/fileextension.py
+++ hackathon-2023-boston-main/Scraper/fileextension.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
+#read extension index
 df = pd.read_csv("C:\\Users\I32800\\Downloads\\hackathon-2023-boston-main\\hackathon-2023-boston-main\\Scraper\\FileTypeLookup.csv",
                  names = ["Extension", "Language"])
 
 def getFileType(filename):
+    #isolate extension, check against index
     fileextension = filename[-(len(filename)-filename.find(".")-1):]
     filetype = df.loc[df.Extension == fileextension, "Language"]
     return filetype
 
+#test file name
 filename = "test.cs"
 
 result = getFileType(filename)
